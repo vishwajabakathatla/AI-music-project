8a3b88938770103370ddcfde43557a08ca5f789e
vishwajabakathatla <88708444+vishwajabakathatla@users.noreply.github.com>
2022-12-26 10:52:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,89 +1,92 @@
-song1 = "";
-song2 = "";
-
-rightWristX = 0;
-rightWristY = 0;
-
-song1_status = "";
-song2_status = "";
+song = "";
+song_2 = "";
+scoreleftWrist = 0;
+scorerightwrist = 0;
+left_play_song = "";
+right_play_song = "";
 
 leftWristX = 0;
 leftWristY = 0;
 
-scoreLeftWrist = 0;
-scoreRightWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
 
-function preload() {
-          song1 = loadSound("music.mp3");
-          song2 = loadSound("music2.mp3");
+function preload(){
+    song = loadSound("music.mp3");
+    song_2 = loadSound("music2.mp3");
+
+
 }
 
-function setup() {
-          canvas = createCanvas(600 , 500);
-          canvas.center()
+function setup(){
+    canvas = createCanvas(500 , 500);
+   canvas.position(500 , 200);
 
-          video = createCapture(VIDEO);
-          video.hide()
 
-          poseNet = ml5.poseNet(video ,modelLoaded);
-          poseNet.on('pose' , gotposes);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoded);
+    poseNet.on('pose' , gotPoses);
 }
 
-function modelLoaded() {
-          console.log("PoseNet is Initialized");
+function modelLoded(){
+
+    console.log('PoseNet is initialized');
+    
 }
+ function gotPoses(results){
+     if(results.length > 0){
+         console.log(results);
 
-function draw() {
-  image(video, 0, 0, 600, 500);
+         scoreleftWrist = results[0].pose.keypoints[9].score;
+         scorerightwrist = results[0].pose.keypoints[10].score;
 
-  song1_status = song1.isPlaying();
-	song2_status = song2.isPlaying();
+         leftWristX = results[0].pose.leftWrist.x;
+         leftWristY = results[0].pose.leftWrist.y;
+         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
 
-	fill("#FF0000");
-	stroke("#FF0000");
+         rightWristX = results[0].pose.rightWrist.x;
+         rightWristY = results[0].pose.rightWrist.y;
+         console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+     }
+ }
+function draw(){
+    image(video , 0 , 0 , 500 , 500);
 
-  if(scoreLeftWrist > 0.2)
-	{
-		circle(leftWristX,leftWristY,20);
+    fill('#fce303');
+    stroke('#fce303');
+    
+    
+    left_play_song = song.isPlaying();
+    right_play_song = song_2.isPlaying();
+    
+    console.log(left_play_song);
 
-			song1.stop();
+    if(scoreleftWrist >0.2){
+        circle(leftWristX , leftWristY , 20);
+       song_2.stop();
 
-		if(song2_status == false)
-		{
-			song2.play();
-			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
-		}
-	}
+       if(left_play_song == false){
+        song.play();
+        document.getElementById("song_name").innerHTML = "Peter Pan";
+    }
+       
+    }
 
-  if(scoreRightWrist > 0.2)
-	{ 
-		circle(rightWristX,rightWristY,20);
+    if(scorerightwrist >0.2){
+        circle(rightWristX , rightWristY , 20);
+       song.stop();
 
-			song2.stop();
+       if(right_play_song == false){
+        song_2.play();
+        document.getElementById("song_name").innerHTML = "Harry Potter";
+    }
+       
+    }
 
-		if(song1_status == false)
-		{
-			song1.play();
-			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
-		}
-	}
-}
 
-function gotposes(results) {
-          if (results.length > 0) {
-            console.log(results);
+   
 
-            scoreLeftWrist = results[0].pose.keypoints[9].score;
-            scoreRightWrist = results[0].pose.keypoints[10].score;
-
-            rightWristX = results[0].pose.rightWrist.x;
-            rightWristY = results[0].pose.rightWrist.y;
-
-            console.log("Right Wrist X = " + rightWristX + " Right Wrist Y = " + rightWristY);
-
-            leftWristX = results[0].pose.leftWrist.x;
-            leftWristY = results[0].pose.leftWrist.y;
-
-            console.log("Left Wrist X = " + leftWristX + " Left Wrist Y = " + leftWristY);
-          }
+   
 }
